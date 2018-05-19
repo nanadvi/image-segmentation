a75d20d662ad2a26eff0b0d4f96e12925d0389e0
nanadvi <nanadvi@ucdavis.edu>
2018-05-18 23:44:27 -0700

    Colorquantization with k-means

---
+++ quantized_RGB.m
@@ -0,0 +1,15 @@
+function [outputImg, meanColors] = quantized_RGB(origImg, k)
+    inputImg = im2double(origImg);
+    [r, c, ~] = size(inputImg); 
+    reshapedImg = reshape(inputImg, r*c, 3);
+    [idx, meanColors] = kmeans(reshapedImg, 3);
+    outputImg_ = zeros(size(reshapedImg));
+    for labels = 1:k
+        outputImg_(idx == labels, 1) = meanColors(labels, 1);
+        outputImg_(idx == labels, 2) = meanColors(labels, 2);
+        outputImg_(idx == labels, 3) = meanColors(labels, 3);
+    end
+    outputImg = reshape(outputImg_, r,c,3);
+end
+
+
