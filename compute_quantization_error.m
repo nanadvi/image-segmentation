b174c38478b4d19761712b66e7f2c3598f3c6290
nanadvi <nanadvi@ucdavis.edu>
2018-05-21 21:25:49 -0700

    Final

--- compute_quantization_error.m
+++
@@ -1,5 +0,0 @@
-function [error] = compute_quantization_error(origImg, quantizedImg)
-    orig = im2double(origImg);
-    quan = im2double(quantizedImg);
-    error = (norm(orig(:)-quan(:),2).^2); 
-end
